3c239a851d07e01d400aaeb44cbcd6555379ae74
SamuelHGerber <henrylightfoot@gmail.com>
2021-05-29 11:11:25 -0700

    feedback message and styling

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-//import { styles } from './App.css';
+import styles from './App.css';
 //import { useRecord } from '../hooks/useRecord';
 
 function App() {
@@ -10,13 +10,14 @@ function App() {
   const [counter, setCounter] = useState(0);
   const [editCounter, setEditCounter] = useState(0);
   const [current, setCurrent] = useState('#FF0000');
+  const [feedback, setFeedback] = useState('');
   //const [undo] = useState(null);
   //const [redo] = useState(null);
 
-  console.log('>>>', colorRecords);
-  console.log('//Current:', current);
-  console.log('|||counter', counter);
-  console.log(':::editCounter', editCounter);
+  // console.log('>>>', colorRecords);
+  // console.log('//Current:', current);
+  // console.log('|||counter', counter);
+  // console.log(':::editCounter', editCounter);
 
   const record = (value) => {
     const currentIndex = colorRecords.length - editCounter;
@@ -40,7 +41,7 @@ function App() {
       //experiment with switching the order of these two calls, looks like sEC happening before sC...
       setEditCounter((editCounter) => editCounter + 1);
       setCurrent(colorRecords[counter - editCounter - 1]);
-    } else console.log('no previous records');
+    } else setFeedback('🤖 -- no previous records');
   };
 
   const redo = () => {
@@ -49,13 +50,14 @@ function App() {
       setEditCounter((editCounter) => editCounter - 1);
       //this part is not working:
       setCurrent(colorRecords[counter - editCounter + 1]);
-    } else console.log('no forward records');
+    } else setFeedback('no forward records -- 🤖');
   };
 
   useEffect(() => {
     if (counter === 0) {
       setColorRecords((colorRecords) => [...colorRecords, current]);
     }
+    setFeedback('>>>');
     setLoading(false);
   }, [current]);
 
@@ -75,24 +77,32 @@ function App() {
 
   return (
     <>
-      <button onClick={undo}>undo</button>
-      <button onClick={redo}>redo</button>
-      <input
-        type="color"
-        value={current}
-        onChange={({ target }) => record(target.value)}
-      />
-      <div
-        style={{ backgroundColor: current, width: '10rem', height: '10rem' }}
-      ></div>
+      <section className={styles.controlArea}>
+        <h3>Select a Color to Change the Square:</h3>
+        <hr></hr>
+        <button onClick={undo}>undo</button>
+        <button onClick={redo}>redo</button>
+        <input
+          type="color"
+          value={current}
+          style={{ margin: '10px' }}
+          onChange={({ target }) => record(target.value)}
+        />
+        <div
+          style={{
+            backgroundColor: current,
+            width: '10rem',
+            height: '10rem',
+            border: 'solid 1px white',
+            padding: '4px',
+          }}
+        ></div>
+      </section>
+      <section className={styles.feedbackArea}>
+        <h4>{feedback}</h4>
+      </section>
     </>
   );
 }
 
 export default App;
-
-// const record = #{
-//   foo: "FOO",
-//   bar: "BAR"
-// };
-// const tuple = #["FOO", "BAR"]
